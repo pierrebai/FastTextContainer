29baaaa0f5129bf297e48393c2294f5063d679c6
Pierre Baillargeon <Pierre.Baillargeon@autolog.com>
2020-03-22 19:24:31 -0400

    Added self-benchmarking code.

--- FastTextContainerPerf/FastTextContainerPerf.cpp
+++ FastTextContainerPerf/FastTextContainerPerf.cpp
@@ -5,9 +5,42 @@
 #include <fstream>
 #include <chrono>
 #include <random>
+#include <functional>
 
 using namespace std;
 using namespace FastTextContainer;
+
+struct BenchmarkInterval
+{
+   BenchmarkInterval() : _start(chrono::steady_clock::now()) { }
+
+   void Restart() { _start = chrono::steady_clock::now(); }
+   void Stop() { _end = chrono::steady_clock::now(); }
+
+   chrono::microseconds GetDuration() const
+   {
+      return chrono::duration_cast<chrono::microseconds>(_end - _start);
+   }
+
+private:
+   chrono::steady_clock::time_point _start;
+   chrono::steady_clock::time_point _end;
+};
+
+chrono::microseconds BenchmarkCode(const function<void()>& func)
+{
+   BenchmarkInterval benchmark;
+   func();
+   benchmark.Stop();
+   return benchmark.GetDuration();
+}
+
+chrono::microseconds PrintBenchmarkCode(const wchar_t* label, const function<void()>& func)
+{
+   auto dur = BenchmarkCode(func);
+   wcout << label << " benchmark: " << dur.count() << "us" << endl;
+   return dur;
+}
 
 template <class HOLDER>
 void CopyHolder(const HOLDER& holder)
@@ -48,35 +81,46 @@ int main(int argc, const char** argv)
    const int seed = atoi(argv[2]);
    const char* holder_type = argv[3];
 
-   if (string("vs") == holder_type)
    {
       VectorOfWStringTextHolder holder;
       {
          wifstream stream(filename);
          holder.ReadLines(stream);
       }
-      //CopyHolder(holder);
-      SelectSomeLines(holder, seed);
+
+      PrintBenchmarkCode(L"Select some lines, vector of wstring", [holder, seed]()
+      {
+         //CopyHolder(holder);
+         SelectSomeLines(holder, seed);
+      });
    }
-   if (string("ds") == holder_type)
+
    {
       DequeOfWStringTextHolder holder;
       {
          wifstream stream(filename);
          holder.ReadLines(stream);
       }
-      //CopyHolder(holder);
-      SelectSomeLines(holder, seed);
+
+      PrintBenchmarkCode(L"Select some lines, deque of wstring", [holder, seed]()
+      {
+         //CopyHolder(holder);
+         SelectSomeLines(holder, seed);
+      });
    }
-   if (string("vb") == holder_type)
+
    {
       VectorOfSharedBuffersTextHolder holder;
       {
          wifstream stream(filename);
          holder.ReadLines(stream);
       }
-      //CopyHolder(holder);
-      SelectSomeLines(holder, seed);
+
+      PrintBenchmarkCode(L"Select some lines, vector of shared buffers", [holder, seed]()
+      {
+         //CopyHolder(holder);
+         SelectSomeLines(holder, seed);
+      });
    }
 
    return 0;
